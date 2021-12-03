1a268f3afe5a3c7714eea5a340460c9527873b4a
Murat Karasah <mkarasah@gmail.com>
2021-12-03 11:24:18 +0300

    Add project files.

---
+++ RecursiveConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RecursiveConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            Console.WriteLine($"{i} sayısının faktoriyeli {faktoriyel(i)}" );
+        }
+        static int faktoriyel(int n)
+        {
+            if (n<=0)
+            {
+                return 1;
+            }
+            else
+            {
+              return  n*faktoriyel(n-1);
+            }
+        }
+    }
+}
